1a016e3fbbe6e73cda35b6b685e1be6219717eda
TomasUNS <31107142+TomasUNS@users.noreply.github.com>
2018-10-16 18:45:37 -0300

    Intento de pipes

--- Sistemas Operativos/Proyecto 2/Secuancias1Pipes.c
+++ Sistemas Operativos/Proyecto 2/Secuancias1Pipes.c
@@ -1,4 +1,10 @@
-#include<stdlib.h>
+#include <stdio.h>
+#include <strings.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 //ABCDE ABCDE ABCDE ABCDE...
 
@@ -16,3 +22,87 @@
 		E hace: down(E), Imprimir, up(A)
 	
 */
+
+void A () {
+	
+}
+
+void B () {
+	
+}
+
+void C () {
+	
+}
+
+void D () {
+	
+}
+
+void E () {
+	
+}
+
+int main () {
+	
+	char writeMessage[20];
+	char readMessage[20];
+	
+	int pdf [2];
+	pipe (pdf);
+	
+    int pid = fork ();
+    if (pid == -1) {
+    	fprintf (stderr,"Error al crear el Proceso Hijo A");
+    }
+    else {
+    	if (pid != 0) {
+        	pid = fork ();
+			if (pid == -1) {
+				fprintf (stderr,"Error al crear el Proceso Hijo B");
+			}
+			else {
+				if (pid == 0) A ();
+				if (pid != 0) {
+					pid = fork ();
+					if (pid == -1) {
+						fprintf (stderr,"Error al crear el Proceso Hijo C");
+					}
+					else {
+						if (pid == 0) B ();
+						if (pid != 0) {
+							pid = fork ();
+							if (pid == -1) {
+								fprintf (stderr,"Error al crear el Proceso Hijo D");
+							}
+							else {
+								if (pid == 0) C ();
+								if (pid != 0) {
+									pid = fork ();
+									if (pid == -1) {
+										fprintf (stderr,"Error al crear el Proceso Hijo E");
+									}
+									else {
+										if (pid == 0) D ();
+										if (pid != 0) {
+											E ();
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+        	exit(0);
+    	}
+    }
+
+	if (pid == 0){
+    	wait(NULL);
+	}
+	
+	
+	
+	return 0;
+}
