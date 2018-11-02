bec73a6cf4861ddc8a147acb10a31b2f8c05313d
Pablo Guillermo <pgcb123@gmail.com>
2018-11-02 18:17:35 -0300

    algo sobre permisos
    tendremos que usar la función de libreria fchmod()

--- Sistemas Operativos/Proyecto 2/Modificadorpermisos.c
+++ Sistemas Operativos/Proyecto 2/Modificadorpermisos.c
@@ -1,1 +1,10 @@
 #include<stdlib.h>
+#include <sys/stat.h>
+#include<fcnt1.h>
+
+//"NO hay que usar Direcciones absolutas": Esto implica que tendriamos que usar fchmod() para modificar los permisos del archivo abierto dado
+
+void main(){
+
+
+}
