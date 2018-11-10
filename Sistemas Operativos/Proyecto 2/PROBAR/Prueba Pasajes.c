37c1e641ac6c925a2b29535ca83448a8e59b9bb2
Pablo Guillermo <pgcb123@gmail.com>
2018-11-10 09:31:41 -0300

    correcciones prueba
    actualizo codigo de prueba de los pasajes Binario/decimal y vice versa

--- Sistemas Operativos/Proyecto 2/PROBAR/Prueba Pasajes.c
+++ Sistemas Operativos/Proyecto 2/PROBAR/Prueba Pasajes.c
@@ -2,7 +2,7 @@
 #include<math.h>
 
 uint16_t DecimalABinario(int n){
-	uint16_t ret = 0x00; //000000000000000
+	uint16_t ret = 0x00; //000000000000000 en binario usando representación en Hexa
 	int aux;
 	int i = 1; //para ir sumando 1, 10, 100, etc
 	while(n > 0){
@@ -19,20 +19,17 @@ uint16_t DecimalABinario(int n){
 /*
  * Dado un Numero binario, lo paso a Decimal, uso método del producto
 */
-int BinarioADecimal(uint8_t n){
-	int i = 7; //acordarse que las posiciones van de 0 a n-1 cuando haciamos las conversiones en Orga
-	int ret = 0;
-	int aux = 0;
-	while(i >= 0){
-		//Agarrar bit por bit y pasarlo a decimal
-		aux = n % 10;
-		ret = ret + (2*pow(aux,i)); /*2*Bit del numero elevado a la i*/
-		n = n / 10;
-		i--;
+int BinarioADecimal(uint16_t n){
+	int rem,decimal,base = 1;
 
+	while(n > 0){
+		rem = n % 10;
+		decimal = decimal + rem * base;
+		n = n / 10;
+		base = base * 2;
 	}
 
-	return ret + 1; //Esto lo teniamos que hacer en Organización si mal no recuerdo... (Representaciones)
+	return decimal;
 }
 
 /*
