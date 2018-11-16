82c5f75a2e57607f1ec3cb9d52e47f5c48e409e2
TomasUNS <31107142+TomasUNS@users.noreply.github.com>
2018-11-16 19:19:57 -0300

    Arreglo sql

--- Bases de Datos/Base de Datos banco/Banco.sql
+++ Bases de Datos/Base de Datos banco/Banco.sql
@@ -670,10 +670,9 @@ CREATE TRIGGER CargoCuotas
 AFTER INSERT ON Prestamo
 FOR EACH ROW
 	BEGIN
-		DECLARE N_P INT;
 		DECLARE I INT DEFAULT 1;
 		#Intento de conseguir el prestamo recien creado
-		WHILE I < NEW.cant_meses DO
+		WHILE I <= NEW.cant_meses DO
 			INSERT INTO pago(nro_prestamo,nro_pago,fecha_venc,fecha_pago) VALUES (NEW.nro_prestamo,I,DATE_ADD(NEW.fecha,INTERVAL I+1 MONTH),NULL);
 			SET I = I + 1;
 		END WHILE;
