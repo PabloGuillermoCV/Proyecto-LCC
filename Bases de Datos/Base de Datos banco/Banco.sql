e9bfb0775f2ad051ad28d8dfd49f294d8bc161c4
TomasUNS <31107142+TomasUNS@users.noreply.github.com>
2018-11-17 10:05:12 -0300

    Modifico trigger

--- Bases de Datos/Base de Datos banco/Banco.sql
+++ Bases de Datos/Base de Datos banco/Banco.sql
@@ -670,10 +670,10 @@ CREATE TRIGGER CargoCuotas
 AFTER INSERT ON Prestamo
 FOR EACH ROW
 	BEGIN
-		DECLARE I INT DEFAULT 1;
+		DECLARE I INT DEFAULT 0;
 		#Intento de conseguir el prestamo recien creado
 		WHILE I <= NEW.cant_meses DO
-			INSERT INTO pago(nro_prestamo,nro_pago,fecha_venc,fecha_pago) VALUES (NEW.nro_prestamo,I,DATE_ADD(NEW.fecha,INTERVAL I+1 MONTH),NULL);
+			INSERT INTO pago(nro_prestamo,nro_pago,fecha_venc,fecha_pago) VALUES (NEW.nro_prestamo,I,DATE_ADD(NEW.fecha,INTERVAL I MONTH),NULL);
 			SET I = I + 1;
 		END WHILE;
 	END; !
